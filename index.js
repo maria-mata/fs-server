616d7f0fd80580b51d2fe716ac2103c2a9821c8b
Maria Mata <mariajo.mata@gmail.com>
2017-07-25 14:29:30 -0600

    Added cors

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 const router = require('./routes')
+const cors = require('cors')
 
+app.use(cors())
 app.use('/', router)
 
 app.listen(port)
