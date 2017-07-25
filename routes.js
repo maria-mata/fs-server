cd0ef4faeec4e7ab7e73e4ea3dfd262cb8058316
Maria Mata <mariajo.mata@gmail.com>
2017-07-25 15:08:00 -0600

    added get request by id parameter

--- routes.js
+++ routes.js
@@ -4,9 +4,16 @@ const knex = require('./db/knex')
 
 router.get('/contacts', (req, res) => {
   knex('*').from('contact')
-  .then(data => {
+    .then(data => {
     res.json(data)
   })
+})
+
+router.get('/contacts/:id', (req, res) => {
+  knex('contact').where('contact.id', req.params.id)
+    .then(data => {
+      res.json(data)
+    })
 })
 
 
